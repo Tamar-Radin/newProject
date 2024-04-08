020290a092add38a9d2d301f4b0158e41e087d9d
Tamar-Radin <r9098051@gmail.com>
2024-04-08 15:41:09 +0300

    erc work

---
+++ src/erc.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK"){
+        this;
+    }
+    function mint(address account, uint256 value) public {
+      _mint(account,value);
+    }
+}
